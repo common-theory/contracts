b789f1a854dbbbdf51a63f4a2016083b0af45794
Chance Hudson <jchancehud@gmail.com>
2018-10-16 14:53:47 -0500

    Clean up index

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { observable } from 'mobx';
 import App from './App';
-
-console.log(web3);
-
-const appState = observable({
-  networkId: -1,
-  totalVotingMembers: 0,
-  members: [],
-
-});
 
 ReactDOM.render(
   <App />,

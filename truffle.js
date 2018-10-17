6f21ac34ea2c3f972034dab6c9f35b1e292c99b8
Chance Hudson <jchancehud@gmail.com>
2018-10-17 14:07:52 -0500

    Add private key deployments for rinkeby

--- truffle.js
+++ truffle.js
@@ -1,9 +1,15 @@
+const PrivateKeyProvider = require('truffle-privatekey-provider');
+
 module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*'
+    },
+    rinkeby: {
+      provider: new PrivateKeyProvider(process.env.RINKEBY_PRIVATE_KEY, 'http://commontheory.io:4545');
+      network_id: '4'
     },
     live: {
       host: 'eth.commontheory.io',

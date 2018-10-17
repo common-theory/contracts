18d9f576cadab1cf7c9be7c12b61c7b591d8679a
Chance Hudson <jchancehud@gmail.com>
2018-10-16 19:36:59 -0500

    Add payable logic and account settling logic

--- contracts/CommonDAC.sol
+++ contracts/CommonDAC.sol
@@ -61,8 +61,11 @@ contract CommonDAC {
   );
 
   mapping (address => Member) public members;
+  address[] memberAddresses;
   mapping (address => Vote[]) public votes;
   mapping (address => mapping (uint => bool)) public memberProposalVotes;
+
+  mapping (address => uint) public balances;
 
   /**
    * Approximately 2 weeks between votes.
@@ -79,6 +82,14 @@ contract CommonDAC {
   bool contractUpdated = false;
   address newContract;
 
+  struct Payment {
+    address sender;
+    uint value;
+    bool settled;
+  }
+
+  Payment[] payments;
+
   constructor(string name, string github, string website) public {
     members[msg.sender] = Member({
       ownership: 1000,
@@ -86,6 +97,7 @@ contract CommonDAC {
       github: github,
       website: website
     });
+    memberAddresses.push(msg.sender);
     totalVotingMembers += 1;
     genesisBlockTimestamp = block.timestamp;
   }
@@ -96,6 +108,61 @@ contract CommonDAC {
    **/
   modifier canVote() {
     if (members[msg.sender].ownership > 0) _;
+  }
+
+  /**
+   * Default payment function. Adds an unsettled payment entry, or forwards the
+   * payment to the updated contract (if an update proposal has passed).
+   **/
+  function() public payable {
+    if (contractUpdated) {
+      newContract.transfer(msg.value);
+    } else {
+      payments.push(Payment({
+        sender: msg.sender,
+        value: msg.value,
+        settled: false
+      }));
+    }
+  }
+
+  /**
+   * Settles all outstanding payments into user balances. Should be used prior
+   * to modifying ownership information to ensure funds are always distributed
+   * using the correct ownership ratio.
+   **/
+  function settleBalances() public canVote {
+    for (uint i = 0; i < payments.length; i++) {
+      if (payments[i].settled) continue;
+      settlePayment(i);
+    }
+  }
+
+  /**
+   * Settles a specific payment into smart contract balances.
+   *
+   * Funds can be withdrawn using the withdraw function below.
+   **/
+  function settlePayment(uint index) public canVote {
+    uint totalDistributedWei = 0;
+    for (uint i = 0; i < memberAddresses.length; i++) {
+      address a = memberAddresses[i];
+      if (members[a].ownership == 0) continue;
+      uint owedWei = payments[index].value * members[a].ownership / totalOwnership;
+      totalDistributedWei += owedWei;
+      balances[a] += owedWei;
+    }
+    assert(totalDistributedWei == payments[index].value);
+    payments[index].settled = true;
+  }
+
+  /**
+   * Withdraw the balance for the calling address.
+   **/
+  function withdraw() public {
+    if (balances[msg.sender] == 0) return;
+    msg.sender.transfer(balances[msg.sender]);
+    balances[msg.sender] = 0;
   }
 
   /**
@@ -165,6 +232,7 @@ contract CommonDAC {
       }
       totalOwnership = totalOwnership - oldOwnership + newOwnership;
       members[proposals[proposalNumber].memberAddress].ownership = newOwnership;
+      memberAddresses.push(proposals[proposalNumber].memberAddress);
     }
 
     // Update the contract address if necessary

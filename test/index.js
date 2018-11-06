47b3437837ad2b8608b7d7d277849eafe1536e88
Chance Hudson <jchancehud@gmail.com>
2018-11-05 23:31:34 -0600

    Add waitForCycle helper function

--- test/index.js
+++ test/index.js
@@ -1,6 +1,26 @@
 const CommonDAC = artifacts.require('CommonDAC');
 const assert = require('assert');
 
+/**
+ * A helper to generate promises that resolve when a certain voting cycle is
+ * active.
+ **/
+const waitForCycle = (cycleNumber, contract) => new Promise(async (rs, rj) => {
+  const currentVoteCycle = +(await contract.currentVoteCycle());
+  const cycleLength = +(await contract.voteCycleLength());
+  const genesisTimestamp = await contract.genesisBlockTimestamp();
+  if (currentVoteCycle > +cycleNumber) {
+    return rj(new Error('Target vote cycle has passed'));
+  } else if (currentVoteCycle == cycleNumber) {
+    return rs();
+  } else {
+    const now = +(new Date() / 1000);
+    const targetTime = cycleLength * cycleNumber + genesisTimestamp;
+    if (targetTime <= now) return rs(targetTime);
+    setTimeout(rs, (targetTime - now) * 1000);
+  }
+});
+
 contract('CommonDAC', accounts => {
 
   it('should initialize with a proposal', async () => {
@@ -13,21 +33,19 @@ contract('CommonDAC', accounts => {
     const contract = await CommonDAC.deployed();
     const oldProposalCount = +(await contract.proposalCount());
     const proposalName = 'test proposal';
+    const genesisTimestamp = await contract.genesisBlockTimestamp();
     await contract.createProposal(proposalName, 0, accounts[1], 50, 0, 0);
     const newProposalCount = oldProposalCount + 1;
     const proposalIndex = newProposalCount - 1;
+    assert.rejects(contract.vote(proposalIndex, false));
     assert.equal(newProposalCount, await contract.proposalCount());
     // proposal is an array of values as defined in the contract
     // abi doesn't automatically map values to keys?
     const proposal = await contract.proposals(proposalIndex);
+    console.log(proposal);
+    console.log(await contract.currentVoteCycle());
     assert.equal(proposal[3], proposalName);
-    const checkForActiveVote = async () => {
-      await new Promise(rs => setTimeout(rs, 1000));
-      return +(await contract.currentVoteCycle()) === +proposal[2];
-    };
-    assert.rejects(contract.vote(proposalIndex, false));
-    // Wait for active vote
-    while (!checkForActiveVote) {}
+    await waitForCycle(proposal[2], contract);
     assert.rejects(contract.applyProposal(proposalIndex));
     assert.doesNotReject(contract.vote(proposalIndex, true));
     await new Promise(rs => setTimeout(rs, 15000));

1f2482835b0f928219317267d2cc356624b423c1
Chance Hudson <jchancehud@gmail.com>
2018-11-05 21:12:49 -0600

    Add WIP proposal passing test

--- test/index.js
+++ test/index.js
@@ -9,4 +9,29 @@ contract('CommonDAC', accounts => {
     assert.notEqual(proposalCount, 0, 'Initial proposal not present.');
   });
 
+  it('should pass a proposal', async () => {
+    const contract = await CommonDAC.deployed();
+    const oldProposalCount = +(await contract.proposalCount());
+    const proposalName = 'test proposal';
+    await contract.createProposal(proposalName, 0, accounts[1], 50, 0, 0);
+    const newProposalCount = oldProposalCount + 1;
+    const proposalIndex = newProposalCount - 1;
+    assert.equal(newProposalCount, await contract.proposalCount());
+    // proposal is an array of values as defined in the contract
+    // abi doesn't automatically map values to keys?
+    const proposal = await contract.proposals(proposalIndex);
+    assert.equal(proposal[3], proposalName);
+    const checkForActiveVote = async () => {
+      await new Promise(rs => setTimeout(rs, 1000));
+      return +(await contract.currentVoteCycle()) === +proposal[2];
+    };
+    assert.rejects(contract.vote(proposalIndex, false));
+    // Wait for active vote
+    while (!checkForActiveVote) {}
+    assert.rejects(contract.applyProposal(proposalIndex));
+    assert.doesNotReject(contract.vote(proposalIndex, true));
+    await new Promise(rs => setTimeout(rs, 15000));
+    assert.equal(await contract.isProposalAccepted(proposalIndex), true);
+  });
+
 });

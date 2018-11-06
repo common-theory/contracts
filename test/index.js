7d30a10b2d02c991fab20da5a5805260c19337a5
Chance Hudson <jchancehud@gmail.com>
2018-11-05 20:17:36 -0600

    async/await syntax in tests

--- test/index.js
+++ test/index.js
@@ -2,12 +2,10 @@ const CommonDAC = artifacts.require('CommonDAC');
 
 contract('CommonDAC', accounts => {
 
-  it('should initialize with a proposal', () => {
-    return CommonDAC.deployed()
-      .then(instance => instance.proposalCount.call())
-      .then(proposalCount => {
-        assert.notEqual(proposalCount, 0, 'Initial proposal not present.');
-      });
+  it('should initialize with a proposal', async () => {
+    const contract = await CommonDAC.deployed();
+    const proposalCount = await contract.proposalCount.call();
+    assert.notEqual(proposalCount, 0, 'Initial proposal not present.');
   });
 
 });

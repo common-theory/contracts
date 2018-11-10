4ee1269fdaa04c86725f899e690772dd1f830688
Chance Hudson <jchancehud@gmail.com>
2018-11-09 21:11:57 -0600

    Improve migrations, deploy all 3 contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,13 +2,19 @@ const CommonSyndicate = artifacts.require('CommonSyndicate');
 const CommonHosting = artifacts.require('CommonHosting');
 const CommonDecision = artifacts.require('CommonDecision');
 
-module.exports = async function(deployer) {
-  const decision = await deployer.deploy(CommonDecision, '0xAb027372B1c52e1615EDdeF59C3Ca4412bf63b9f', 30).await;
-  const syndicate = await deployer.deploy(CommonSyndicate, decision.address).await;
-  const hosting = await deployer.deploy(CommonHosting, decision.address).await;
-  console.log(`Deployed all:
-decision contract: ${decision.address}
-syndicate contract: ${syndicate.address}
-hosting contract: ${hosting.address}
-`);
+/**
+ * async/await and Promise.* functions do not work for the promises below
+ * https://github.com/trufflesuite/truffle/issues/501#issuecomment-332589663
+ **/
+module.exports = function(deployer) {
+  deployer.deploy(CommonDecision, '0xAb027372B1c52e1615EDdeF59C3Ca4412bf63b9f', 30)
+    .then(() => deployer.deploy(CommonSyndicate, CommonDecision.address))
+    .then(() => deployer.deploy(CommonHosting, CommonDecision.address))
+    .then(() => console.log(`
+Deployed the following:
+
+decision contract: ${CommonDecision.address}
+syndicate contract: ${CommonSyndicate.address}
+hosting contract: ${CommonHosting.address}
+`));
 };

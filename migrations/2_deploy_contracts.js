1876f3d518b89927604fb973a72b034a3e3cb37b
Chance Hudson <jchancehud@gmail.com>
2018-11-09 20:15:22 -0600

    Add deployment for decision, syndicate, and hosting

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
-const CommonDAC = artifacts.require('CommonDAC');
+const CommonSyndicate = artifacts.require('CommonSyndicate');
+const CommonHosting = artifacts.require('CommonHosting');
+const CommonDecision = artifacts.require('CommonDecision');
 
-module.exports = function(deployer) {
-  deployer.deploy(CommonDAC, '0xAb027372B1c52e1615EDdeF59C3Ca4412bf63b9f', 30);
+module.exports = async function(deployer) {
+  const decision = await deployer.deploy(CommonDecision, '0xAb027372B1c52e1615EDdeF59C3Ca4412bf63b9f', 30).await;
+  const [ syndicate, hosting ] = Promise.all([
+    deployer.deploy(CommonSyndicate, decision).await,
+    deployer.deploy(CommonHosting, decision).await,
+  ]);
 };

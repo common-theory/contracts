32fd76cd176d8a7b14281a58d7f6136b2ca3ffa9
Chance Hudson <jchancehud@gmail.com>
2018-11-09 20:32:33 -0600

    Add deployment for new contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,11 @@ const CommonDecision = artifacts.require('CommonDecision');
 
 module.exports = async function(deployer) {
   const decision = await deployer.deploy(CommonDecision, '0xAb027372B1c52e1615EDdeF59C3Ca4412bf63b9f', 30).await;
-  const [ syndicate, hosting ] = Promise.all([
-    deployer.deploy(CommonSyndicate, decision).await,
-    deployer.deploy(CommonHosting, decision).await,
-  ]);
+  const syndicate = await deployer.deploy(CommonSyndicate, decision.address).await;
+  const hosting = await deployer.deploy(CommonHosting, decision.address).await;
+  console.log(`Deployed all:
+decision contract: ${decision.address}
+syndicate contract: ${syndicate.address}
+hosting contract: ${hosting.address}
+`);
 };

99e1c1d57e7baac35373640a6d9c756456a600f7
Chance Hudson <jchancehud@gmail.com>
2018-12-28 15:50:14 -0600

    Refactor contract deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,17 @@
-const CommonSyndicate = artifacts.require('CommonSyndicate');
-const CommonHosting = artifacts.require('CommonHosting');
-const CommonDecision = artifacts.require('CommonDecision');
-const StringUtils = artifacts.require('StringUtils');
+const Syndicate = artifacts.require('Syndicate');
+const Decision = artifacts.require('Decision');
 
 /**
  * async/await and Promise.* functions do not work for the promises below
  * https://github.com/trufflesuite/truffle/issues/501#issuecomment-332589663
  **/
 module.exports = function(deployer) {
-  deployer.deploy(StringUtils)
-    .then(() => deployer.link(StringUtils, [CommonDecision, CommonHosting]))
-    .then(() => deployer.deploy(CommonDecision, '0xAb027372B1c52e1615EDdeF59C3Ca4412bf63b9f', 30))
-    .then(() => deployer.deploy(CommonSyndicate, CommonDecision.address))
-    .then(() => deployer.deploy(CommonHosting, CommonDecision.address))
+  deployer.deploy(Syndicate)
+    .then(() => deployer.deploy(Decision, Syndicate.address))
     .then(() => console.log(`
 Deployed the following:
 
-decision contract: ${CommonDecision.address}
-syndicate contract: ${CommonSyndicate.address}
-hosting contract: ${CommonHosting.address}
+syndicate contract: ${Syndicate.address}
+decision contract: ${Decision.address}
 `));
 };

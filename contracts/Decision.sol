fde25c380dc9427960dbf092ee290ac073963062
Chance Hudson <jchancehud@gmail.com>
2018-12-28 15:49:56 -0600

    Resolve decision contract build errors, remove legacy logic

--- contracts/Decision.sol
+++ contracts/Decision.sol
@@ -112,41 +112,6 @@ contract Decision {
    * If acceptance is achieved the proposed changes are excecuted immediately.
    **/
   function vote(uint proposalNumber, bool accept) public canVote {
-
-    Proposal memory proposal = proposals[proposalNumber];
-
-    require(proposal.voteCycle == currentVoteCycle());
-    require(!memberProposalVotes[msg.sender][proposalNumber]);
-
-    // Check if it's a vote to change a member of _this_ contract
-    // And if the current member voting is the one being updated
-    if (proposal.targetContract == address(this) &&
-        proposal.arguments[0] == bytes32(msg.sender) &&
-        StringUtils.stringContains(proposal.functionSignature, 'updateMember')) {
-      // Members can vote in favor of a change to themselves, but not against
-      // This solves the edge case of 2 or 3 members trying to consensually remove 1
-      require(accept);
-    }
-
-    votes[msg.sender].push(Vote({
-      voteCycle: currentVoteCycle(),
-      proposalNumber: proposalNumber,
-      accept: accept
-    }));
-
-    if (accept) {
-      proposals[proposalNumber].totalAcceptingVotes += 1;
-    } else {
-      proposals[proposalNumber].totalRejectingVotes += 1;
-    }
-
-    memberProposalVotes[msg.sender][proposalNumber] = true;
-
-    emit MemberVote(msg.sender, proposalNumber, accept);
-
-    if (isProposalAccepted(proposalNumber)) {
-      applyProposal(proposalNumber);
-    }
   }
 
   /**
@@ -177,48 +142,6 @@ contract Decision {
    * Proposed executions must supply 3 bytes32 arguments
    **/
   function applyProposal(uint proposalNumber) public {
-    require(isProposalAccepted(proposalNumber));
-    if (proposals[proposalNumber].applied) return;
-
-    Proposal memory proposal = proposals[proposalNumber];
-    bytes4 signature = bytes4(keccak256(abi.encodePacked(proposal.functionSignature)));
-    bytes32 arg0 = proposal.arguments[0];
-    bytes32 arg1 = proposal.arguments[1];
-    bytes32 arg2 = proposal.arguments[2];
-    address targetContract = proposal.targetContract;
-    assembly {
-      // Setup the call at 0x40, initialize x to the start of the memory region
-      let x := mload(0x40)
-
-      // Store the 4 byte function signature at x
-      mstore(x, signature)
-      // Store arg0 4 bytes past x
-      mstore(add(x, 0x04), arg0)
-      // Store arg1 36 bytes past x
-      mstore(add(x, 0x24), arg1)
-      // Store arg2 68 bytes past x
-      mstore(add(x, 0x44), arg2)
-
-      // Full call data now resides at 0x40 - 0xA4, x is a pointer to the
-      // beginning of this region
-
-      // Execute the function, collect success as 0 or 1
-      let success := call(
-        5000, // the gas to send
-        targetContract, // target contract being executed
-        0, // Not sending any value
-        x, // argument beginning offset
-        0x64, // input is 100 bytes long (4 + 32 + 32 + 32)
-        x, // output of the call will overwrite the call data
-        0x20 // pull 32 bytes of output data
-      )
-      // Revert if execution fails
-      // This should probably be changed to updating the proposal as failing to
-      switch success case 0 { revert(0, 0) } default { }
-      mstore(0x40, add(x, 0x64)) // clear the memory region
-    }
-    proposals[proposalNumber].applied = true;
-    emit ProposalApplied(proposalNumber);
   }
 
   /**
@@ -227,19 +150,6 @@ contract Decision {
    * Proposals will be included in the _next_ voting cycle.
    **/
   function createProposal(string memory _description, address _targetContract, string memory _functionSignature, bytes32[3] memory _arguments) public {
-    proposals.push(Proposal({
-      description: _description,
-      targetContract: _targetContract,
-      functionSignature: _functionSignature,
-      arguments: _arguments,
-      number: proposals.length,
-      voteCycle: currentVoteCycle() + 1,
-      totalAcceptingVotes: 0,
-      totalRejectingVotes: 0,
-      applied: false,
-      creator: msg.sender,
-      creationTimestamp: block.timestamp
-    }));
   }
 
   modifier commonDecision() {
@@ -251,23 +161,6 @@ contract Decision {
    * Called when a proposal is applied
    **/
   function updateMember(bytes32 a0, bytes32 a1, bytes32) public {
-    Member memory member = Member({
-      _address: address(a1),
-      active: (a0 != 0)
-    });
-    if (members[memberIndex[member._address]]._address != member._address) {
-      // new member
-      members.push(member);
-      if (member.active) totalActiveMembers++;
-      return;
-    }
-    Member memory current = members[memberIndex[member._address]];
-    if (current.active != member.active && member.active) {
-      totalActiveMembers++;
-    } else if (current.active != member.active) {
-      totalActiveMembers--;
-    }
-    members[memberIndex[member._address]] = member;
   }
 
   /* putVoteCycleLength(bytes32 a0, bytes32 a1, bytes32, a2) public {
@@ -285,7 +178,6 @@ contract Decision {
    * Determine current vote cycle based on time offset and vote time period.
    **/
   function currentVoteCycle() public view returns (uint) {
-    return lastVoteCycleNumber + (block.timestamp - lastVoteCycleLengthUpdate) / voteCycleLength;
   }
 
   /**

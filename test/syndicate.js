c417da84d510de36d923a0211eccc87b6948c858
Chance Hudson <jchancehud@gmail.com>
2018-12-30 19:51:38 -0600

    Add instant payment deposit test

--- test/syndicate.js
+++ test/syndicate.js
@@ -1,12 +1,24 @@
-const Syndicate = artifacts.require('CommonSyndicate');
+const Syndicate = artifacts.require('Syndicate');
 const assert = require('assert');
 
 contract('Syndicate', accounts => {
 
   it('should initialize', async (...args) => {
     console.log(args);
+    console.log(accounts);
     const contract = await Syndicate.deployed();
     assert.equal(true, true);
+  });
+
+  it('should deposit an instant payment', async () => {
+    const contract = await Syndicate.deployed();
+    const owner = accounts[0];
+    await contract.deposit(owner, 0, {
+      from: owner,
+      value: 100,
+      gasLimit: contract.deposit.estimateGas(owner, 0)
+    });
+    assert.equal(100, await contract.balances(owner));
   });
 
 });

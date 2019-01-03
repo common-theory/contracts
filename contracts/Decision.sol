fb3908744fb45a9e19245508c50284f5cb03fa11
Chance Hudson <jchancehud@gmail.com>
2019-01-03 00:32:43 -0600

    Refactor decision implementation

--- contracts/Decision.sol
+++ contracts/Decision.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.5.0;
 
-import './StringUtils.sol';
 import './Syndicate.sol';
 
 /**
@@ -11,4 +10,49 @@ import './Syndicate.sol';
  **/
 
 contract Decision {
+
+  struct ProposedPayment {
+    address payable receiver;
+    uint256 _time;
+    uint256 weiValue;
+  }
+
+  struct Proposal {
+    ProposedPayment[] paymentProposals;
+    uint256 votingMembers;
+    uint256 votes;
+  }
+
+  Proposal[] proposals;
+
+  address payable syndicateAddress;
+
+  constructor(address payable _syndicateAddress) public {
+    syndicateAddress = _syndicateAddress;
+  }
+
+  function canExecuteProposal(uint256 index) public view returns (bool) {
+    require(index >= 0);
+    require(index < proposals.length);
+    Proposal memory proposal = proposals[index];
+    uint256 totalWei = 0;
+    for (uint256 i = 0; i < proposal.paymentProposals.length; i++) {
+      totalWei += proposal.paymentProposals[i].weiValue;
+    }
+    return totalWei <= address(this).balance;
+  }
+
+  function executeProposal(uint256 index) public {
+    require(canExecuteProposal(index));
+    Proposal memory proposal = proposals[index];
+    Syndicate syndicate = Syndicate(syndicateAddress);
+    for (uint256 i = 0; i < proposal.paymentProposals.length; i++) {
+      ProposedPayment memory payment = proposal.paymentProposals[i];
+      syndicate.deposit.value(payment.weiValue)(payment.receiver, payment._time);
+    }
+  }
+
+  function proposalCount() public view returns (uint256) {
+    return proposals.length;
+  }
 }
